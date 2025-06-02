744c6a9a5b298aa70597a6d760d0d865a8c726d9
Eri-158 <erick.mera.santander@utelvt.edu.ec>
2025-06-02 09:50:04 -0500

    Update miscript.js

--- miscript.js
+++ miscript.js
@@ -1,3 +1,61 @@
+const apiButton = document.getElementById("apiButton");
+const apiData = document.getElementById("apiData");
+const base_experience = document.getElementById("base_experience");
+
+const callAPI = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon/ditto")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            apiData.innerText = JSON.stringify(data);
+            base_experience.innerText = `Experiencia Base= ${JSON.stringify(data.base_experience)}`
+        })
+        .catch(error => {
+            console.error("Error al llamar a la API:", error);
+        });
+};
+apiButton.addEventListener("click", callAPI);
+
+function buscar() {
+  const texto = document.getElementById("cajaBusqueda").value.toLowerCase(); // nombre del Pokémon
+  const resultado = document.getElementById("resultado");
+  const imagen = document.getElementById("imagenResultado");
+  const base_experience = document.getElementById("base_experience");
+
+  resultado.innerText = `Buscaste: "${texto}"`;
+
+  fetch(`https://pokeapi.co/api/v2/pokemon/${texto}`)
+    .then(res => {
+      if (!res.ok) {
+        throw new Error("Pokémon no encontrado");
+      }
+      return res.json();
+    })
+    .then(data => {
+      const sprite = data.sprites.front_default;
+      imagen.src = sprite;
+      imagen.style.display = "block";
+      base_experience.innerText = `Experiencia Base: ${data.base_experience}`;
+    })
+    .catch(error => {
+      resultado.innerText = `Error: ${error.message}`;
+      imagen.style.display = "none";
+      base_experience.innerText = "";
+    });
+}
+function enviar_formulario(){
+    var radio=document.querySelector('input[name=radio]:checked').value;
+
+    let checkboxes = document.querySelectorAll('input[name="check"]:checked');
+    let check_seleccionaados = [];
+
+    checkboxes.forEach((checkbox)=>{
+        check_seleccionaados.push(checkbox.value);
+    });
+    var select = document.getElementById("select").value;
+
+    alert(radio + "\n"+ check_seleccionaados +"\n"+ select);
+}
 function sumar(){
     var n1 = parseInt(document.getElementById("n1").value);
     var n2 = parseInt(document.getElementById("n2").value);
@@ -14,6 +72,7 @@ function obtener_texto2(){
     var a = document.getElementById("h2").textContent;
     alert(a)
 }
+
 
 function agregar(){ //sustituye lo que hay dentro del div
     document.getElementById("div1").innerHTML = "Hola <input type='text'>";
@@ -58,4 +117,7 @@ document.getElementById("quitar_clase").addEventListener("click", function(){
 });
 
 
-
+document.querySelector("#asdfg").onclick =() =>{
+    const nombre = "Juan";
+    alert("Bienvenido, "+ nombre);
+};
